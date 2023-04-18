7a453fbb8908da32255099b93aa068af04f8c109
KangMinJin <kmj_7809@naver.com>
2023-04-18 15:19:44 +0900

    0418 src파일 업로드

---
+++ Project/src/db_common.php
@@ -0,0 +1,35 @@
+<?php
+    //---------------------------------
+    //함수명    : db_conn
+    //기능      : php를 db에 연결
+    //파라미터  : array(속성)    $param_conn(파라미터 명)
+    //---------------------------------
+    
+    function db_conn( &$param_conn )
+    {
+        $host = "localhost";
+        $user = "root";
+        $pass = "root506";
+        $charset = "utf8mb4";
+        $db_name = "to_do_list";
+        $dns = "mysql:host=".$host.";dbname=".$db_name.";charset=".$charset;
+        $pdo_option =
+            array(
+                PDO::ATTR_EMULATE_PREPARES      => false
+                ,PDO::ATTR_ERRMODE              => PDO::ERRMODE_EXCEPTION
+                ,PDO::ATTR_DEFAULT_FETCH_MODE   => PDO::FETCH_ASSOC
+            );
+
+        try
+        {
+            $param_conn = new PDO( $dns, $user, $pass, $pdo_option );
+        }
+        catch (Exception $e)
+        {
+            $param_conn = null;
+            throw new Exception( $e->getMessage() );
+        }
+    }
+
+
+?>
