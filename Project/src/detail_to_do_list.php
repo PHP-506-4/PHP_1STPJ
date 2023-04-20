48d89e5929bc30594631b768b83091cc6325ba7b
KangMinJin <kmj_7809@naver.com>
2023-04-20 10:42:37 +0900

    0420 디테일페이지 css 작성, 통합 css 추가, 프로필 사진 색깔 변경

--- Project/src/detail_to_do_list.php
+++ Project/src/detail_to_do_list.php
@@ -18,22 +18,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>리스트 상세 페이지</title>
     <link rel="stylesheet" href="./common/css_common.css">
+    <link rel="stylesheet" href="./common/css_detail.css">
 <body>
     <div class="main">
         <div class="con">
             <?php include_once( HEADER ); ?>
             <br>
             <?php include_once( PROFILE ) ?>
-            <div class=con1>
-                <p><?php echo $result_info["list_title"]; ?></p>
+            <div class="con1">
+                <h2>리스트 상세</h2>
+                <div class="list_d1">
+                    <p><?php echo $result_info["list_title"]; ?></p>
+                </div>
                 <span>공부 시간</span>
-                <span><?php echo sprintf('%02d',$result_info["list_start_time"]); ?>:</span>
-                <span><?php echo sprintf('%02d',$result_info["list_start_minute"]); ?> ~ </span>
-                <span><?php echo sprintf('%02d',$result_info["list_end_time"]); ?>:</span>
-                <span><?php echo sprintf('%02d',$result_info["list_end_minute"]); ?></span>
-                <p><?php echo $result_info["list_memo"]; ?></p>
-                <a href="update_to_do_list.php?list_no=<?php echo $result_info["list_no"];?>">수정</a>
-                <a href="to_do_list.php">리스트로</a>
+                <div class="list_d2">
+                    <span><?php echo sprintf('%02d',$result_info["list_start_time"]); ?>:</span>
+                    <span><?php echo sprintf('%02d',$result_info["list_start_minute"]); ?> ~ </span>
+                    <span><?php echo sprintf('%02d',$result_info["list_end_time"]); ?>:</span>
+                    <span><?php echo sprintf('%02d',$result_info["list_end_minute"]); ?></span>
+                </div>
+                <div class="list_d3">
+                    <p><?php echo $result_info["list_memo"]; ?></p>
+                </div>
+                <div class="con_btn">
+                    <a href="update_to_do_list.php?list_no=<?php echo $result_info["list_no"];?>" class="btn1">수정</a>
+                    <a href="to_do_list.php" class="btn2">리스트로</a>
+                </div>
             </div>
         </div>
     </div>
