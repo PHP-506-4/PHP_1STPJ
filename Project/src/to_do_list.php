4abc74b7af30c31e484f457a08420a822decbc15
KangMinJin <kmj_7809@naver.com>
2023-04-20 15:21:10 +0900

    0420 list css 수정

--- Project/src/to_do_list.php
+++ Project/src/to_do_list.php
@@ -18,6 +18,8 @@ $result_list_info = select_list_info();
     <title>to do list</title>
     <link rel="stylesheet" href="./common/css_common.css">
     <link rel="stylesheet" href="./common/css_goal_to_do_list.css">
+    <link rel="stylesheet" href="./common/css_to_do_list.css">
+
 </head>
 <body>
     <div class="main">
@@ -28,57 +30,50 @@ $result_list_info = select_list_info();
             <div class="con1">
                 <?php include_once( GOAL )?>
                 <div class="clr">
-                    <table>
                         <?php
                         foreach ($result_list_info as $val)
                         {
                         ?>
-                            <tr>
-                                <td>
-                                <?php $val["list_no"]?>
-                                </td>
-                                <td>
-                                <?php
-                                $comp_flg = $val["list_comp_flg"];
-                                if($comp_flg === '0' )
-                                {
+                            <div class="list">
+                                <div class="chk">
+                                    <?php
+                                    $comp_flg = $val["list_comp_flg"];
+                                    if($comp_flg === '0' )
+                                    {
+                                        ?>
+                                        <img src="./img/check.png" alt="체크 안함">
+                                    <?php
+                                    }
+                                    else
+                                    {
+                                        ?>
+                                        <img src="./img/checked.png" alt="체크 함">
+                                    <?php
+                                    }
                                     ?>
-                                    <img src="./img/check.png" alt="체크 안함">
-                                <?php
-                                }
-                                else
-                                {
-                                    ?>
-                                    <img src="./img/checked.png" alt="체크 함">
-                                <?php
-                                }
-                                ?>
-                                </td>
-                                <td>
+                                </div>
                                 <a href="detail_to_do_list.php?list_no=<?php echo $val["list_no"]?>"><?php echo $val["list_title"]?></a>
-                                </td>
-                                <td>
-                                <?php
-                                if($val["list_start_time"]!== "" && $val["list_start_minute"]!== "")
-                                {
-                                echo sprintf("%02d",$val["list_start_minute"])." : ".sprintf("%02d",$val["list_start_minute"]);
-                                ?>
-                                <span> ~ </span>
-                                <?php
-                                }
-                                ?>
-                                <?php
-                                if($val["list_end_time"]!== "" && $val["list_end_minute"]!== "")
-                                {
-                                echo sprintf("%02d",$val["list_end_time"])." : ".sprintf("%02d",$val["list_end_minute"]);
-                                }
-                                ?>
-                                </td>
-                            </tr>
+                                <div class="time">
+                                    <?php
+                                    if($val["list_start_time"]!== "" && $val["list_start_minute"]!== "")
+                                    {
+                                    echo sprintf("%02d",$val["list_start_minute"])." : ".sprintf("%02d",$val["list_start_minute"]);
+                                    ?>
+                                    <span> ~ </span>
+                                    <?php
+                                    }
+                                    ?>
+                                    <?php
+                                    if($val["list_end_time"]!== "" && $val["list_end_minute"]!== "")
+                                    {
+                                    echo sprintf("%02d",$val["list_end_time"])." : ".sprintf("%02d",$val["list_end_minute"]);
+                                    }
+                                    ?>
+                                </div>
+                            </div>
                         <?php
                         }
                         ?>
-                    </table>
                 </div>
             </div>
         </div>
