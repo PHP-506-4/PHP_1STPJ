82d5dd3d95e1138822716a467ae12720a1495292
KangMinJin <kmj_7809@naver.com>
2023-04-24 09:57:48 +0900

    0424 주석 추가

--- Project/src/detail_to_do_list.php
+++ Project/src/detail_to_do_list.php
@@ -22,14 +22,18 @@
     <link rel="stylesheet" href="./common/css_detail.css">
 <body>
         <div class="con">
+            <!-- 헤더 -->
             <?php include_once( HEADER ); ?>
             <br>
+            <!-- 프로필 -->
             <?php include_once( PROFILE ) ?>
             <div class="con1">
                 <h2>리스트 상세</h2>
                 <div class="list_d1">
+                    <!-- 리스트 제목 출력 -->
                     <p><?php echo $result_info["list_title"]; ?></p>
                 </div>
+                <!-- 공부 시작, 끝 시간 출력 -->
                 <span>공부 시간</span>
                 <div class="list_d2">
                     <span><?php echo sprintf('%02d',$result_info["list_start_time"]); ?> : </span>
@@ -37,9 +41,11 @@
                     <span><?php echo sprintf('%02d',$result_info["list_end_time"]); ?> : </span>
                     <span><?php echo sprintf('%02d',$result_info["list_end_minute"]); ?></span>
                 </div>
+                <!-- 메모 출력 -->
                 <div class="list_d3">
                     <p><?php echo $result_info["list_memo"]; ?></p>
                 </div>
+                <!-- 수행완료 / 취소 버튼 -->
                 <div class="con_btn">
                     <?php
                     $comp_flg = $result_info["list_comp_flg"];
@@ -60,6 +66,7 @@
                 </div>
             </div>
         </div>
+        <!-- 푸터 -->
     <?php include_once( FOOTER ); ?>
 </body>
 </html>
