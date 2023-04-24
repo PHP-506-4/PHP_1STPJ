43499460254adf293da259b047c85a14966d5907
baechanghyeon12 <qo99033@naver.com>
2023-04-24 10:19:35 +0900

    0424 delete 주석 수정

--- Project/src/delete_to_do_list.php
+++ Project/src/delete_to_do_list.php
@@ -1,14 +1,14 @@
 <?php
-define("DB_CON",$_SERVER["DOCUMENT_ROOT"]."/Project/src");                           /* 정확한 파일의 루트가 정해지면 수정 */
+define("DB_CON",$_SERVER["DOCUMENT_ROOT"]."/Project/src");                          
 define("URL",DB_CON."/common/db_common.php");
 define( "URL_HEADER", DB_CON."/header_to_do_list.php" );
 define( "PROFILE", DB_CON."/profile_to_do_list.php" );
 define( "FOOTER", DB_CON."/footer_to_do_list.php");
 include_once(URL);
 
-$list_no = $_GET["list_no"];
+$list_no = $_GET["list_no"];                            
 
-$result_title = select_list_no( $list_no );
+$result_title = select_list_no( $list_no );                             /* get으로 받은 list_no를 조회 */
 
 ?>
 
@@ -18,28 +18,28 @@ $result_title = select_list_no( $list_no );
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="연결할 CSS파일 경로">                          <!-- CSS파일이 정해지면 경로 수정 -->
+    <link rel="stylesheet" href="연결할 CSS파일 경로">
     <title>삭제 페이지</title>
     <link rel="stylesheet" href="./common/css_common.css">
     <link rel="stylesheet" href="./delete.css">
 </head>
 <body>
     <div class="con">
-        <?php include_once(URL_HEADER);?>                                                     <!-- 헤더 영역이 정해지면 include_once로 설정  -->
+        <?php include_once(URL_HEADER);?>                                                               <!-- 헤더부분 연결 -->
         <br>
-        <?php include_once( PROFILE ) ?>
+        <?php include_once( PROFILE ) ?>                                                                <!-- 프로필부분 연결 -->
         <div class="con1">
             <h2>리스트 삭제</h2>
-            <p class="title_p"><?php echo $result_title["list_title"] ?></p>                                                          <!-- PK로 넘어오는 값을 받아서 해당 PK의 제목을 화면에 표시 -->
+            <p class="title_p"><?php echo $result_title["list_title"] ?></p>                                                          <!-- GET으로 넘어오는 값을 받아서 해당 list_no의 list_title을 화면에 표시 -->
             <p class="exception_p">이 리스트를 삭제합니다.<br>동의 하시면 확인을 눌러 주세요.</p>                     <!-- 주의 메세지 -->
                 <a class="check" href=" sub_delete.php?list_no=<?php echo $list_no ?>" >
-                    확인                                                                <!-- 클릭시 삭제를 완료하고 리스트 페이지로 이동(삭제 페이지를 하나 더 만들어야 된다.) -->
-                </a>                                                                    <!-- 그 페이지로 넘어가서 삭제를 하고 리스트 페이지로 바로 넘어갈 수 있게 만들어야 된다. -->
+                    확인                                                                <!-- sub_delete.php로 가서 삭제하고 to_do_list로 이동 -->
+                </a>
             <a class="cancel" href="detail_to_do_list.php?list_no=<?php echo $list_no ?>" >
-                    취소                                                                <!-- 클릭시 상세 페이지로 이동(상세 페이지의 URL을 작성) -->
+                    취소                                                                <!-- 클릭시 상세 페이지로 이동 -->
                 </a>
         </div>
     </div>
-    <?php include_once( FOOTER ); ?>
+    <?php include_once( FOOTER ); ?>                                                    <!-- footer부분 include_once로 연결 -->
 </body>
 </html>
